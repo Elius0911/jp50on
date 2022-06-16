2f9d69ebcc7615a39d815b8bd79ecbbf3b8e5b00
Zypheus <c25887@gmail.com>
2022-06-16 17:44:26 +0800

    table finish, list finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
+import 'wordList.dart';
+
+WordList wordList = WordList();
 void main() {
   runApp(const MyApp());
 }
@@ -7,109 +11,192 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: SafeArea(
+          child: HomePage(),
+        ));
+  }
+}
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  Widget wordCard(int num) {
+    return SizedBox(
+      height: 60,
+      width: 73,
+      child: TextButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Colors.white),
+        ),
+        clipBehavior: Clip.antiAlias, // 抗鋸齒
+        child: Container(
+          alignment: Alignment.center,
+          child: Column(
+            children: [
+              Text(
+                wordList.wordList[num].JPword,
+                style: TextStyle(color: Colors.black, fontSize: 20),
+              ),
+              Text(
+                wordList.wordList[num].roman,
+                style: TextStyle(color: Colors.black, fontSize: 13),
+              ),
+            ],
+          ),
+        ),
+        onPressed: () {},
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+/*
+  int index = 0;
+  Widget LoadWord5(int number) {
+    for (int i = 0; i < 5; i++) {
+      return wordCard(i);
+    }
+    index += 5;
   }
 
+  Widget LoadWord3(int number) {
+    for (int i = 0; i < 3; i++) {
+      wordCard(i);
+      index += 3;
+    }
+  }
+
+  Widget LoadWord2(int number) {
+    for (int i = 0; i < 2; i++) {
+      wordCard(i);
+      index += 2;
+    }
+  }
+*/
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Container(
+      color: Colors.grey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          /*
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              LoadWord5(index),
+            ],
+          ),
+          */
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              wordCard(0),
+              wordCard(1),
+              wordCard(2),
+              wordCard(3),
+              wordCard(4),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              wordCard(5),
+              wordCard(6),
+              wordCard(7),
+              wordCard(8),
+              wordCard(9),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              wordCard(10),
+              wordCard(11),
+              wordCard(12),
+              wordCard(13),
+              wordCard(14),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              wordCard(15),
+              wordCard(16),
+              wordCard(17),
+              wordCard(18),
+              wordCard(19),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              wordCard(20),
+              wordCard(21),
+              wordCard(22),
+              wordCard(23),
+              wordCard(24),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              wordCard(25),
+              wordCard(26),
+              wordCard(27),
+              wordCard(28),
+              wordCard(29),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              wordCard(30),
+              wordCard(31),
+              wordCard(32),
+              wordCard(33),
+              wordCard(34),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              wordCard(35),
+              wordCard(36),
+              wordCard(37),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              wordCard(38),
+              wordCard(39),
+              wordCard(40),
+              wordCard(41),
+              wordCard(42),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              wordCard(43),
+              wordCard(44),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              wordCard(45),
+            ],
+          )
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ lib/wordList.dart
@@ -0,0 +1,57 @@
+class Word {
+  String JPword;
+  String roman;
+
+  Word(this.JPword, this.roman);
+}
+
+class WordList {
+  List<Word> wordList = [
+    Word("あ", "a"), //a
+    Word("い", "i"),
+    Word("う", "u"),
+    Word("え", "e"),
+    Word("お", "o"),
+    Word("か", "ka"), //ka
+    Word("き", "ki"),
+    Word("く", "ku"),
+    Word("け", "ke"),
+    Word("こ", "ko"),
+    Word("さ", "sa"), //sa
+    Word("し", "shi"),
+    Word("す", "su"),
+    Word("せ", "se"),
+    Word("そ", "so"),
+    Word("た", "ta"), //ta
+    Word("ち", "chi"),
+    Word("つ", "tsu"),
+    Word("て", "te"),
+    Word("と", "to"),
+    Word("な", "na"), //na
+    Word("に", "ni"),
+    Word("ぬ", "nu"),
+    Word("ね", "ne"),
+    Word("の", "no"),
+    Word("は", "ha"), //ha
+    Word("ひ", "hi"),
+    Word("ふ", "fu"),
+    Word("へ", "he"),
+    Word("ほ", "ho"),
+    Word("ま", "ma"), //ma
+    Word("み", "mi"),
+    Word("む", "mu"),
+    Word("め", "me"),
+    Word("も", "mo"),
+    Word("や", "ya"), //ya
+    Word("ゆ", "yu"),
+    Word("よ", "yo"),
+    Word("ら", "ra"), //ra
+    Word("り", "ri"),
+    Word("る", "ru"),
+    Word("れ", "re"),
+    Word("ろ", "ro"),
+    Word("わ", "wa"), //wa
+    Word("を", "wo"),
+    Word("ン", "n"), //n
+  ];
+}
